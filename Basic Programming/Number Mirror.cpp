ede6ff5397ff881c81b3648b60c5fb8f479d9b95
YashGupta018 <ygupta2001@gmail.com>
2022-12-02 11:29:51 +0530

    Basic Programming Day-1

---
+++ Basic Programming/Number Mirror.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define w(t) ll t; cin>>t; while(t--)
+#define mod 1000000007
+using namespace std;
+
+int main()
+{
+	fast_io
+	    int n;
+	    cin>>n;
+	    cout<<n<<endl;
+	return 0;
+}
