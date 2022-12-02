99638d0b3c515879aee2bc19237512a0207fd5f2
YashGupta018 <ygupta2001@gmail.com>
2022-12-02 14:05:49 +0530

    Few Corrections

--- Basic Programming/Enormous Input Test.cpp
+++ Basic Programming/Enormous Input Test.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
@@ -10,12 +9,10 @@ int main()
     cin >> n >> k;
 
     int count=0;
-    for (int i=0;i<=n-1;i++)
-    {
+    for (int i=0;i<=n-1;i++) {
         int a;
         cin >> a;
-        if (a%k==0)
-        {
+        if (a%k==0) {
             count++;
         }
     }
