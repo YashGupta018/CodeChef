d11ea128e47d4eb5fadd4ec8ba4463a0b321f7df
YashGupta018 <ygupta2001@gmail.com>
2022-12-10 21:44:06 +0530

    Practice Question

---
+++ Practice/Bella ciao.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int nt;
+	cin>>nt;
+	while(nt--) {
+	    long long int D,d,p,q;
+	    cin>>D>>d>>p>>q;
+	    int n=D/d;
+	    long long int money=p*d*n+(q*(n-1)*d*n)/2;
+	    long long int rem=D%d;
+	    money+=rem*(p+n*q);
+	    cout<<money<<endl;
+	}
+	return 0;
+}
